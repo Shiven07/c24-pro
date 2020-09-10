e5850f6d5ea0711a47f537b678ab8f60db446860
Shiven07 <62198795+Shiven07@users.noreply.github.com>
2020-09-10 20:23:54 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,3 +1,4 @@
+var paper, ground, leftsidebox, rightsidebox, basebox;
 
 const Engine = Matter.Engine;
 const World = Matter.World;
@@ -10,17 +11,29 @@ function preload()
 }
 
 function setup() {
-	createCanvas(800, 700);
+	createCanvas(1000, 700);
 
-	ellipse(56, 46, 55, 55);
-
-	
 	engine = Engine.create();
 	world = engine.world;
 
+	//ellipse(56, 46, 55, 55);
+  //Matter.Bodies.circle(200, 200, 30)
 	//Create the Bodies Here.
+  ground = new Ground(500,height,1000,20)
 
+  //leftsidebox=createSprite(500, 610, 20, 100);
+	//leftsidebox.shapeColor=color(255, 0, 0);
 
+	//basebox=createSprite(610, 650, 170, 20);
+	//basebox.shapeColor=color(255, 0, 0);
+
+	//rightsidebox=createSprite(675, 610, 20, 100);
+  //rightsidebox.shapeColor=color(255, 0, 0);
+  
+  paperObject = new Paper(100, 300, 30);
+
+  dustbinobject = new Dustbin(600, 600);
+  
 	Engine.run(engine);
   
 }
@@ -29,10 +42,19 @@ function setup() {
 function draw() {
   rectMode(CENTER);
   background(0);
-  
+  ground.display();
+  paperObject.display();
+  dustbinobject.display();
   drawSprites();
  
 }
 
 
+function keypressed()  {
+if (keyCode === UP_ARROW) {
 
+Matter.Body.applyForce(paperObject.body,paperObject.body.position, {x:85,y:-85});
+
+}
+
+}
